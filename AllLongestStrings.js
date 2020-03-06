c4d2473204506a8f256d6de2647e2e8f9759a465
Santiago <greminoficial@gmail.com>
2020-03-06 10:52:51 -0600

    AllLongestString added

---
+++ AllLongestStrings.js
@@ -0,0 +1,29 @@
+/**
+ * INSTRUCTIONS
+ * Given an array of strings, return another array containing all of its longest strings.
+
+    For inputArray = ["aba", "aa", "ad", "vcd", "aba"], 
+    the output should be
+
+    allLongestStrings(inputArray) = ["aba", "vcd", "aba"].
+
+ */
+
+
+
+function allLongestStrings(inputArray) {
+    let output = [inputArray[0]];
+    for(let i = 1; i < inputArray.length; i++) if(inputArray[i].length > output[0].length)
+        output = [inputArray[i]];
+    else if(inputArray[i].length === output[0].length) output.push(inputArray[i])
+
+    return output;
+}
+
+
+console.log(
+    allLongestStrings(["abc",
+        "eeee",
+        "abcd",
+        "dcd"])
+);
