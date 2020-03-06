30e1179a66074352027c459887d265c3dc27d468
Santiago <greminoficial@gmail.com>
2020-03-06 11:54:22 -0600

    AllLongestString added

--- AllLongestStrings.js
+++ AllLongestStrings.js
@@ -13,9 +13,9 @@
 
 function allLongestStrings(inputArray) {
     let output = [inputArray[0]];
-    for(let i = 1; i < inputArray.length; i++) if(inputArray[i].length > output[0].length)
+    for (let i = 1; i < inputArray.length; i++) if (inputArray[i].length > output[0].length)
         output = [inputArray[i]];
-    else if(inputArray[i].length === output[0].length) output.push(inputArray[i])
+    else if (inputArray[i].length === output[0].length) output.push(inputArray[i])
 
     return output;
 }
@@ -27,3 +27,14 @@ console.log(
         "abcd",
         "dcd"])
 );
+
+
+// THIS IS THE BEST WAY I FOUND IN CODEPEN
+
+/**
+     function allLongestStrings(inputArray) {
+        'use strict';
+        let maxSize = Math.max(...inputArray.map(x => x.length));
+        return inputArray.filter(x => x.length === maxSize);
+    }
+ */
